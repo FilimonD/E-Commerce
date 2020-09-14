08f9fd81b7238bc17c18b3e97a3b63149b851198
abnk068 <filimon.daniel68@gmail.com>
2020-09-14 14:36:29 +0100

    updated catr reducer

--- src/components/storeContext.js
+++ src/components/storeContext.js
@@ -3,22 +3,21 @@ import React, { useReducer } from "react";
 const StoreContext = React.createContext();
 const initialState = [];
 
+const findItemIndex = (state, product) =>
+  product.size
+    ? state.findIndex(
+        item => item.id === product.id && item.size === product.size
+      )
+    : product.color
+    ? state.findIndex(
+        item => item.id && product.id && item.color === product.color
+      )
+    : state.findIndex(item => item.id === product.id);
+
 function cartReducer(state, action) {
   switch (action.type) {
     case "add":
-      const itemIndex = state.findIndex(item => {
-        if (item.size) {
-          return (
-            item.id === action.product.id && item.size === action.product.size
-          );
-        } else if (item.color) {
-          return (
-            item.id === action.product.id && item.color === action.product.color
-          );
-        } else {
-          return item.id === action.product.id;
-        }
-      });
+      const itemIndex = findItemIndex(state, action.product);
 
       if (itemIndex === -1) {
         return [...state, action.product];
@@ -28,18 +27,19 @@ function cartReducer(state, action) {
           name: action.product.name,
           size: action.product.size,
           quantity: action.product.quantity + state[itemIndex].quantity,
-          price:
-            action.product.price *
-            (action.product.quantity + state[itemIndex].quantity)
+          price: action.product.price
+          // price:
+          //   action.product.price *
+          //   (action.product.quantity + state[itemIndex].quantity)
         };
-
-        const updatedStore = [...state.splice(itemIndex, 1, newItem)];
-
+        const updatedStore = [
+          ...state.slice(0, itemIndex),
+          ...state.slice(itemIndex + 1, state.length),
+          newItem
+        ];
         return updatedStore;
       }
-    // if(item.name === product.name) {
-    //   return quantity = quantity += 1
-    // }
+
     case "remove":
       const productIndex = state.findIndex(
         item => item.name === action.product.name
