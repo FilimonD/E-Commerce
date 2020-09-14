61e95e1f6cb6c216624497a1f7eb7b050e38eb38
abnk068 <filimon.daniel68@gmail.com>
2020-09-14 15:50:57 +0100

    Updates

---
+++ precache-manifest.f67841c17f961df08bac7b6a2242931d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bf1a2e301942d2bb69c1c980246cfa6",
+    "url": "/E-Commerce/index.html"
+  },
+  {
+    "revision": "547bc0fa80ac61b131f1",
+    "url": "/E-Commerce/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "57329e82a9031e3d3a54",
+    "url": "/E-Commerce/static/css/main.7668649c.chunk.css"
+  },
+  {
+    "revision": "547bc0fa80ac61b131f1",
+    "url": "/E-Commerce/static/js/2.cc559fce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/E-Commerce/static/js/2.cc559fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57329e82a9031e3d3a54",
+    "url": "/E-Commerce/static/js/main.cdcb0866.chunk.js"
+  },
+  {
+    "revision": "0ac642f299a53c4ee393",
+    "url": "/E-Commerce/static/js/runtime-main.21c8859a.js"
+  },
+  {
+    "revision": "d4346e075f44cc1fd61ee8e4d4bf5f1a",
+    "url": "/E-Commerce/static/media/bg.d4346e07.jpeg"
+  }
+]);
